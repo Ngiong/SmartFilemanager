7aee00db748c31b8ad3ba956711892f88bfa6106
Robert Sebastian Herlim <ngiooong@yahoo.com>
2017-11-09 22:31:57 +0700

    Add Test Summarization

---
+++ src/smart/summarizer/DocumentsSummarizer.py
@@ -0,0 +1,10 @@
+from gensim.summarization import keywords
+
+
+class DocumentsSummarizer(object):
+    def __init__(self, documents):
+        self.documents = documents
+
+    def getSummarization(self):
+        concatenated_document = ' '.join(self.documents)
+        return keywords(concatenated_document)
