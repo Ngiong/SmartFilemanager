3e90029d09ccfd4938114e224289957753451596
varian97 <variancaesar@gmail.com>
2017-11-09 13:36:48 +0700

    input for stemmer changed to token

--- src/smart/preprocessor/DocumentStemmer.py
+++ src/smart/preprocessor/DocumentStemmer.py
@@ -1,11 +1,15 @@
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 class DocumentStemmer(object):
-    def __init__(self, document):
-        self.document = document
+    def __init__(self, token):
+        self.token = token
 
     def stem(self):
+        result = []
         factory = StemmerFactory()
         stemmer = factory.create_stemmer()
-        self.document = stemmer.stem(self.document)
-        return self.document
+
+        for item in self.token:
+            result.append(stemmer.stem(item))
+
+        return result
--- test/DocumentStemmerTest.py
+++ test/DocumentStemmerTest.py
@@ -1,10 +1,12 @@
 from src.smart.preprocessor import DocumentStemmer
+from src.smart.preprocessor import DocumentTokenizer
 
 class DocumentStemmerTest(object):
     def test_stemmer (self):
         document = "Bangsa Indonesia sedang bersedih karena kehilangan salah satu musisi terbaiknya." \
                    "Karena begitu sedihnya, Indonesia dihibur oleh negara tetangga."
-        stemmer = DocumentStemmer(document)
+        tokenizer = DocumentTokenizer(document)
+        stemmer = DocumentStemmer(tokenizer.getTokens())
         result = stemmer.stem()
         print(result)
 
